d10e191ff70c468c3bdc2e8a2fa1ebe57150291f
Silvio173 <silvio.sgmtec@gmail.com>
2021-08-04 01:41:54 -0300

    Primeiro commit da IDE Eclipse

---
+++ src/PrmeiroPrograma.java
@@ -0,0 +1,11 @@
+
+public class PrmeiroPrograma {
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;		
+		
+		Gato gato = new Gato("Silvio Gatao", "Negro", "47");
+		
+		System.out.println(gato);		
+	}
+}
